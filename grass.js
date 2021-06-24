5932ca9fe08c964443a7caa5a65ff77605335299
GrishaAdamyan <g.adamyan2004@gmail.com>
2021-06-24 16:38:03 +0400

    Add

--- grass.js
+++ grass.js
@@ -4,7 +4,7 @@ module.exports = class Grass extends main {
     constructor(x, y, index, multiply) {
         super(x, y, index, multiply);
     }
-    
+
     mul() {
         this.multiply++;
         var emptyCells = super.chooseCell(0);
@@ -17,6 +17,22 @@ module.exports = class Grass extends main {
             var newGrass = new Grass(newCell[0], newCell[1]);
             GrassArr.push(newGrass);
             this.multiply = 0;
+        }
+        if (weath == "winter") {
+            this.energy -= 2;
+            this.multiply -= 2;
+        }
+        if (weath == "spring") {
+            this.energy += 5;
+            this.multiply += 5;
+        }
+        if (weath == "summer") {
+            this.energy += 3;
+            this.multiply += 3;
+        }
+        if (weath == "autumn") {
+            this.energy--;
+            this.multiply--;
         }
     }
 }
