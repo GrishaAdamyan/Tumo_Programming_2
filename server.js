9500c0ed07994380378a5e9d32b1191d96505d0d
GrishaAdamyan <g.adamyan2004@gmail.com>
2021-06-24 15:55:57 +0400

    add

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var server = require('http').createServer(app);
+io = require('socket.io')(server);
 var fs = require("fs");
 
 app.use(express.static("."));
