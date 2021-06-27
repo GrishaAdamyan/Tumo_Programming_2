0293876197daad0de54e9cc29f979bdef6978bd4
GrishaAdamyan <g.adamyan2004@tumo.org>
2021-06-27 11:23:52 +0400

    changes

--- grass.js
+++ grass.js
@@ -1,38 +1,35 @@
 let main = require('./main')
 
 module.exports = class Grass extends main {
-    constructor(x, y, index, multiply) {
-        super(x, y, index, multiply);
-    }
 
     mul() {
         this.multiply++;
-        var emptyCells = super.chooseCell(0);
-        var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
-
-        console.log(emptyCells);
-        if (newCell && this.multiply >= 5) {
-            matrix[newCell[0]][newCell[1]] = 1;
-
-            var newGrass = new Grass(newCell[0], newCell[1]);
-            GrassArr.push(newGrass);
-            this.multiply = 0;
-        }
-        if (weath == "winter") {
-            this.energy -= 2;
-            this.multiply -= 2;
-        }
-        if (weath == "spring") {
-            this.energy += 5;
-            this.multiply += 5;
-        }
-        if (weath == "summer") {
-            this.energy += 3;
-            this.multiply += 3;
-        }
-        if (weath == "autumn") {
-            this.energy--;
-            this.multiply--;
+        if (this.multiply >= 5) {
+            let emptyCells = super.chooseCell(0)
+            let newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+            if (this.multiply >= 5 && newCell) {
+                let x = newCell[0]
+                let y = newCell[1]
+                matrix[y][x] = 1
+                grassArr.push(new Grass(x, y, 1))
+                this.multiply = 0;
+            }
+            if (weath == "winter") {
+                this.energy -= 2;
+                this.multiply -= 2;
+            }
+            if (weath == "spring") {
+                this.energy += 5;
+                this.multiply += 5;
+            }
+            if (weath == "summer") {
+                this.energy += 3;
+                this.multiply += 3;
+            }
+            if (weath == "autumn") {
+                this.energy--;
+                this.multiply--;
+            }
         }
     }
 }
